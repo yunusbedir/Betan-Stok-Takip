a09d616c9e0ae98f57b80ad82840a13f7dd756aa
yunusbedir <yunusbedir@outlook.com.tr>
2024-11-25 00:06:14 +0300

    update colors

--- app/src/main/java/com/betan/betanstoktakip/MainActivity.kt
+++ app/src/main/java/com/betan/betanstoktakip/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.betan.betanstoktakip
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.betan.betanstoktakip.databinding.ActivityMainBinding
@@ -28,13 +25,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
         Firebase.auth.addAuthStateListener {
             setupNavigation()
         }
